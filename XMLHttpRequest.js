ab20ce86cafa5acfd7b2e91b0afc20eb262ed03f
Aminmoharramishizari <aminmoharami@gmail.com>
2023-06-13 15:58:08 +0330

    XHR "GET"  Method

--- XMLHttpRequest.js
+++ XMLHttpRequest.js
@@ -1,9 +1,30 @@
-    function XHRGET() {
-        const Ajax = new XMLHttpRequest();
-        var items = GetReferredIds();
-        Ajax.open('GET', '/Secretariat/SecretariatDocuments/GetAttachZipFile?ReferredID=' + items.join(','));
-        Ajax.onreadystatechange=(function () {
-            console.log(this)
-        })
-        Ajax.send();
+
+function Xhr() {
+    const Ajax = new XMLHttpRequest();
+    var items = GetReferredIds();
+    var link = '/Secretariat/SecretariatDocuments/GetAttachZipFile?ReferredID=' + items.join(',');
+
+
+    Ajax.onreadystatechange = function () {
+        console.log(this);
     }
+
+    Ajax.open('GET', link);
+    console.log("OPENED", Ajax.readyState); // readyState will be 1
+    AjaxLoading();
+
+    Ajax.onprogress = () => {
+        console.log("LOADING", Ajax.readyState); // readyState will be 3
+        AjaxLoading();
+    };
+
+    Ajax.onload = () => {
+        downloadLink = document.createElement('a');
+        downloadLink.href = link;
+        downloadLink.click();
+        downloadLink.remove();
+        console.log("DONE", Ajax.readyState); // readyState will be 4
+        AjaxHideLoading();
+    };
+    Ajax.send();
+}
