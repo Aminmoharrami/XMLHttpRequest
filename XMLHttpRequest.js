ddd379c15926a4795fe2b9222dc205241ca3b775
Aminmoharramishizari <aminmoharami@gmail.com>
2023-06-14 08:29:10 +0330

    1402/03/24 Management Request by GET

--- XMLHttpRequest.js
+++ XMLHttpRequest.js
@@ -1,23 +1,30 @@
 
+
+
+//Management Request by GET
 function Xhr() {
     const Ajax = new XMLHttpRequest();
-    var items = GetReferredIds();
-    var link = '/Secretariat/SecretariatDocuments/GetAttachZipFile?ReferredID=' + items.join(',');
+    var link = '/Secretariat/SecretariatDocuments/GetAttachZipFile?ReferredID=';
 
-
+//Check readyState in Console (XMLHttpRequest)
     Ajax.onreadystatechange = function () {
         console.log(this);
     }
 
+    //Open 
     Ajax.open('GET', link);
     console.log("OPENED", Ajax.readyState); // readyState will be 1
     AjaxLoading();
 
+
+    //Handale ReadyState Loading
     Ajax.onprogress = () => {
         console.log("LOADING", Ajax.readyState); // readyState will be 3
         AjaxLoading();
     };
 
+
+     //Handale ReadyState Done
     Ajax.onload = () => {
         downloadLink = document.createElement('a');
         downloadLink.href = link;
@@ -26,5 +33,7 @@ function Xhr() {
         console.log("DONE", Ajax.readyState); // readyState will be 4
         AjaxHideLoading();
     };
+
+    //Send Request
     Ajax.send();
 }
